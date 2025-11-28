a63051b85eb22e10961595371e029f814b0ad9ae
OpadijoIdris <Opadijoayomipo@gmail.com>
2025-11-28 07:38:39 +0100

    work starts

---
+++ src/model/userModel.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true, 
+        trim: true,
+        select: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true, 
+        unique: true, 
+        trim: true
+    },
+    role: {
+        type: String,
+        enum: ["learner", "instructor", "admin"],
+        default: "leaner"
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    skill: [
+        {
+            type: String,
+        }
+    ]
+
+}, { timestamps: true});
+
+const User = model("User", userSchema);
+
+export default User;
